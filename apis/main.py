7ba7b6736d15c4a1921c24c54e714c10b6372f55
Obersand <121788640+Obersand@users.noreply.github.com>
2025-04-30 15:39:57 +0100

    feat: parse iCalendar events (#54)
    * Add parsing of iCalendar events using https://icspy.readthedocs.io/en/stable/index.html.
    
    Events starting before the current date and from which no ticketing link can be extracted are skipped.
    
    Signed-off-by: Jeffrey Belt <jeffrey@theshifters.ch>
    
    * Add workshops which publish to the iCalendar format.
    
    These are Planet C, Fresque du Sol and Fresque des Déchets.
    
    Signed-off-by: Jeffrey Belt <jeffrey@theshifters.ch>
    
    * Fresque des Dechets already has ID 15.
    
    Signed-off-by: Jeffrey Belt <jeffrey@theshifters.ch>
    
    * Restore Fresque des Dechets with proper ID, declare ICS dependency to Nix.
    
    Signed-off-by: Jeffrey Belt <jeffrey@theshifters.ch>
    
    * Add missing closing quote.
    
    Signed-off-by: Jeffrey Belt <jeffrey@theshifters.ch>
    
    ---------
    
    Signed-off-by: Jeffrey Belt <jeffrey@theshifters.ch>

--- apis/main.py
+++ apis/main.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
+from apis.ics import get_ics_data
 from apis.glorieuses import get_glorieuses_data
 
-APIS_FNS = {"hook.eu1.make.com": get_glorieuses_data}
+APIS_FNS = {
+    "hook.eu1.make.com": get_glorieuses_data,
+    "calendar.google.com/calendar/ical": get_ics_data,
+    "framagenda.org/remote.php/dav": get_ics_data,
+}
 
 
 def main(apis):
