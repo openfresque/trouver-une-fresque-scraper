b6e0a5b701dbf70ddaab9b4865168d3c4fe09123
Jeffrey Belt <jeffrey@theshifters.ch>
2025-05-17 19:28:16 +0000

    Add all date string comments as test cases.
    I am considering rewriting parts of the code (to reuse regular expressions or switch to https://dateparser.readthedocs.io/en/latest/ to read dates in any language) and want some guarantee that I won't break anything.
    
    Signed-off-by: Jeffrey Belt <jeffrey@theshifters.ch>

--- utils/date_and_time_test.py
+++ utils/date_and_time_test.py
@@ -8,16 +8,64 @@ from utils import date_and_time
 def run_tests():
     test_cases = [
         (
-            "June 3 English",
+            "BilletWeb: one hour",
+            "Thu Oct 19, 2023 from 01:00 PM to 02:00 PM",
+            datetime(2023, 10, 19, 13, 0),
+            datetime(2023, 10, 19, 14, 0),
+        ),
+        (
+            "BilletWeb: multiple months",
+            "Thu Oct 19, 2023 at 01:00 PM to Sat Feb 24, 2024 at 02:00 PM",
+            datetime(2023, 10, 19, 13, 0),
+            datetime(2024, 2, 24, 14, 0),
+        ),
+        (
+            "BilletWeb: single date and time",
+            "March 7, 2025 at 10:00 AM",
+            datetime(2025, 3, 7, 10, 0),
+            datetime(2025, 3, 7, 13, 0),
+        ),
+        (
+            "EventBrite",
+            "ven. 11 avr. 2025 14:00 - 17:30 CEST",
+            datetime(2025, 4, 11, 14, 0),
+            datetime(2025, 4, 11, 17, 30),
+        ),
+        (
+            "FdC French",
+            "16 mai 2025, de 18h30 à 21h30 (heure de Paris)",
+            datetime(2025, 5, 16, 18, 30),
+            datetime(2025, 5, 16, 21, 30),
+        ),
+        (
+            "FdC English: June 3",
             "June 03, 2025, from 05:30pm to 09:30pm (Paris time)",
             datetime(2025, 6, 3, 17, 30),
             datetime(2025, 6, 3, 21, 30),
         ),
         (
-            "October 28 English",
+            "FdC English: October 28",
             "October 28, 2025, from 09:00am to 12:00pm (Zürich time)",
             datetime(2025, 10, 28, 9, 0),
             datetime(2025, 10, 28, 12, 0),
+        ),
+        (
+            "FEC",
+            "03 mars 2025, 14:00 – 17:00 UTC+1",
+            datetime(2025, 3, 3, 14, 0),
+            datetime(2025, 3, 3, 17, 0),
+        ),
+        (
+            "Glide",
+            "mercredi 12 février 2025 de 19h00 à 22h00",
+            datetime(2025, 2, 12, 19, 0),
+            datetime(2025, 2, 12, 22, 0),
+        ),
+        (
+            "HelloAsso",
+            "Le 12 février 2025, de 18h à 20h",
+            datetime(2025, 2, 12, 18, 0),
+            datetime(2025, 2, 12, 20, 0),
         ),
     ]
     for test_case in test_cases:
