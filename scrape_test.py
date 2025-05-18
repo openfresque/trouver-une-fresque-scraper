0205e877416f8d4481b975b764f269b5f15021a8
Jeffrey Belt <jeffrey@theshifters.ch>
2025-05-18 19:36:21 +0000

    fix: parse dates in English from the Climate Fresk website
    Signed-off-by: Jeffrey Belt <jeffrey@theshifters.ch>

--- scrape_test.py
+++ scrape_test.py
@@ -1,7 +1,9 @@
 from apis import ics_test
+from utils import date_and_time_test
 from utils import language_test
 
 
 if __name__ == "__main__":
     ics_test.run_tests()
+    date_and_time_test.run_tests()
     language_test.run_tests()
